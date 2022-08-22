e6f834ec0bdda73a16b95c07d9c1fa2480569a4b
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-08-22 23:20:11 +0900

    adding TODOLIST

--- MOMENTUM/js/todo.js
+++ MOMENTUM/js/todo.js
@@ -2,10 +2,23 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    console.log(li);
+    toDoList.appendChild(li);
+
+}
+
 function handleToDoSubmit(e) {
     e.preventDefault(); //submit의 기본동작 새로고침 방지
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newTodo);
 }
 
-toDoForm.addEventListener("submit", handleToDoSubmit); //submit을 하면 handleToDoSubmit이라는 함수를 실행
+toDoForm.addEventListener("submit", handleToDoSubmit); 
+//submit을 하면 handleToDoSubmit이라는 함수를 실행
