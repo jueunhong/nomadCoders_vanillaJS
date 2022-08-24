ee700a101bb57714d6524f48bd5fc51b485c24e6
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-08-24 16:41:36 +0900

    Deleting ToDos

--- MOMENTUM/js/todo.js
+++ MOMENTUM/js/todo.js
@@ -3,15 +3,24 @@ const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
 
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
-    console.log(li);
+    const button = document.createElement("button");
+    button.innerText = "✖"
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 
 }
+
 
 function handleToDoSubmit(e) {
     e.preventDefault(); //submit의 기본동작 새로고침 방지
